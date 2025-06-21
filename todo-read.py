7a1ef3112b79de9dad6f76e9e753010315b6e190
Sujit Chaurasia <sujitchaurasia@Sujits-MacBook-Air.local>
2025-06-21 12:57:57 +0530

    Add following questions todo

---
+++ todo-read.py
@@ -0,0 +1,16 @@
+'''
+
+    🔍 Expert Interview Questions You Might Be Asked
+        How does your parser handle invalid cron strings?
+
+        Why does cron allow both day of week and day of month, and how does it behave if both are given?
+
+        How would you extend this to support non-standard fields like seconds or named months?
+
+        Could you make this parser reusable as a Python module for other projects?
+
+        Can this logic be generalized for other interval expressions like ISO 8601 repeating intervals?
+
+        What are the performance implications of your current approach?
+
+'''
